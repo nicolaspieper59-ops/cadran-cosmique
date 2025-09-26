f876bdeeb2bd5143598635c1c2f251ab259427f1
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-26 11:21:41 +0000

    Update moteur.js

--- js/moteur.js
+++ js/moteur.js
@@ -1,21 +1,18 @@
-function appliquerDégradé() {
+window.onload = async () => {
   const canvas = document.getElementById("cadran-cosmique");
   const ctx = canvas.getContext("2d");
   const latitude = 43.6119;
   const longitude = 3.8777;
-  const now = new Date();
-  const depression = calculerDépressionSolaire(now, latitude, longitude);
-  const couleur = couleurSelonDépression(depression);
 
-  ctx.fillStyle = couleur;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  function afficherCadran() {
+    const now = new Date();
+    const depression = calculerDépressionSolaire(now, latitude, longitude);
+    const couleur = couleurSelonDépression(depression);
+    ctx.fillStyle = couleur;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    afficherConstellations(ctx);
+  }
 
-  ctx.beginPath();
-  ctx.arc(canvas.width / 2, canvas.height / 2, 80, 0, 2 * Math.PI);
-  ctx.strokeStyle = "#fff";
-  ctx.lineWidth = 1;
-  ctx.stroke();
-}
-
-setInterval(appliquerDégradé, 60000);
-window.onload = appliquerDégradé;
+  afficherCadran();
+  setInterval(afficherCadran, 60000);
+};
