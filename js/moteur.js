f2894cec44fee0be225856abade41007c6dedaf5
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-26 14:03:23 +0000

    Update moteur.js

--- js/moteur.js
+++ js/moteur.js
@@ -1,18 +1,8 @@
-window.onload = async () => {
+import { horlogeMinecraft } from "./horloge-minecraft.js";
+import { convertirUnites } from "./unite-cosmique.js";
+import { lireCapteurs } from "./capteurs.js";
+import { localisation } from "./gps.js";
+
+window.onload = () => {
   const canvas = document.getElementById("cadran-cosmique");
-  const ctx = canvas.getContext("2d");
-  const latitude = 43.6119;
-  const longitude = 3.8777;
-
-  function afficherCadran() {
-    const now = new Date();
-    const depression = calculerDépressionSolaire(now, latitude, longitude);
-    const couleur = couleurSelonDépression(depression);
-    ctx.fillStyle = couleur;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    afficherConstellations(ctx);
-  }
-
-  afficherCadran();
-  setInterval(afficherCadran, 60000);
-};
+  const ctx = canvas.getContext("2d
