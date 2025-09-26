cc27b7224efbfb5f53336970d3a8b00032c35cf8
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-26 12:55:54 +0000

    Update and rename js/sidereal.js to horloge-minecraft.js

---
+++ horloge-minecraft.js
@@ -0,0 +1,7 @@
+export function horlogeMinecraft() {
+  const now = new Date();
+  const h = now.getHours().toString().padStart(2, "0");
+  const m = now.getMinutes().toString().padStart(2, "0");
+  const s = now.getSeconds().toString().padStart(2, "0");
+  return `${h}:${m}:${s}`;
+}
